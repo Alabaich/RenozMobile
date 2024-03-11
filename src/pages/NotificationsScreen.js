871371b2ec675e9644ca0da336054e9a4cbd9e9f
Alabaich <130333013+Alabaich@users.noreply.github.com>
2024-03-11 10:34:07 -0400

    working version

--- src/pages/NotificationsScreen.js
+++ src/pages/NotificationsScreen.js
@@ -1,13 +1,10 @@
 // In ./src/pages/HomeScreen.tsx
 import React from 'react';
 import { View, Text } from 'react-native';
-import AllProductsComponent from '../components/shopifyInitialisation';
-import LoginScreen from './LoginScreen';
 
 export const NotificationsScreen = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', background: "#fff", }}>
     <Text>Notifications Screen </Text>
-    <LoginScreen />
   </View>
 
 );
